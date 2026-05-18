134b72e1860840471fc38a9bede69ef354ffacb3
MTV <matveivadimovih@gmail.com>
2026-05-18 23:59:42 +0300

    new file:   src/core/memoize.js

---
+++ src/core/memoize.js
@@ -0,0 +1,38 @@
+const argKey = (x) => String(x)+':'+typeof x;
+const generateKey = (args) => args.map(argKey).join('|');
+
+function memo(fn, maxSize = Infinity, evict = null)
+{
+    const cache = new Map();
+
+    return function(...args)
+    {
+        const key = generateKey(args);
+
+        if(cache.has(key))
+        {
+            const entry = cache.get(key);
+            entry.count++;
+            cache.delete(key);
+            cache.set(key, entry);
+            return entry.result;
+        }
+
+        const res = fn(...args);
+
+        if(cache.size >= maxSize)
+        {
+            if(typeof evict === 'function')
+            {
+                evict(cache);
+            }
+            if (cache.size >= maxSize)
+            {
+                const firstKey = cache.keys().next().value;
+                cache.delete(firstKey);
+            }
+        }
+        cache.set(key, {result: res, count: 1, createTime: Date.now()});
+        return res;
+    }
+}
